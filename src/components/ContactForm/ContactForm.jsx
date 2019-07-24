404d846b23f29a3d60b17ee798fd158516373ac1
Alisa Filatova <alicefill@bk.ru>
2019-07-24 17:30:05 +0300

    fixed indents

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -11,36 +11,36 @@ const FORM_FIELDS = [
     name: 'name',
     placeholder: 'Your name',
     required: true,
-		action: this.onNameChange,
+    action: this.onNameChange,
   },
-	{
-		type: 'email',
-		name: 'email',
-		placeholder: 'E-mail',
-		required: true,
-		action: this.onEmailChange,
-	},
-	{
-		type: 'tel',
-		name: 'phone',
-		placeholder: 'Phone',
-		required: false,
-		action: this.onTelChange,
-	},
-	{
-		type: 'text',
-		name: 'location',
-		placeholder: 'Event location',
-		required: false,
-		action: this.onLocationChange,
-	},
-	{
-		type: 'text',
-		name: 'date',
-		placeholder: 'Event date',
-		required: false,
-		action: this.onDateChange,
-	},
+  {
+    type: 'email',
+    name: 'email',
+    placeholder: 'E-mail',
+    required: true,
+    action: this.onEmailChange,
+  },
+  {
+    type: 'tel',
+    name: 'phone',
+    placeholder: 'Phone',
+    required: false,
+    action: this.onTelChange,
+  },
+  {
+    type: 'text',
+    name: 'location',
+    placeholder: 'Event location',
+    required: false,
+    action: this.onLocationChange,
+  },
+  {
+    type: 'text',
+    name: 'date',
+    placeholder: 'Event date',
+    required: false,
+    action: this.onDateChange,
+  },
 ];
 
 
@@ -50,113 +50,113 @@ const api = new API({endPoint: END_POINT, authorization: AUTHORIZATION});
 
 class ContactForm extends React.Component {
 
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			showAlert: false,
-			sendStatus: '',
-			userName: '',
-			email: '',
-			tel: '',
-			location: '',
-			date: '',
-			message: '',
-		}
-	}
+    this.state = {
+      showAlert: false,
+      sendStatus: '',
+      userName: '',
+      email: '',
+      tel: '',
+      location: '',
+      date: '',
+      message: '',
+    }
+  }
 
-	onNameChange = (event)=> {
-		this.setState({userName: event.target.value});
-	};
+  onNameChange = (event)=> {
+    this.setState({userName: event.target.value});
+  };
 
-	onEmailChange = (event)=> {
-		this.setState({email: event.target.value});
-	};
+  onEmailChange = (event)=> {
+    this.setState({email: event.target.value});
+  };
 
-	onTelChange = (event)=> {
-		this.setState({tel: event.target.value});
-	};
+  onTelChange = (event)=> {
+    this.setState({tel: event.target.value});
+  };
 
-	onLocationChange = (event)=> {
-		this.setState({location: event.target.value});
-	};
+  onLocationChange = (event)=> {
+    this.setState({location: event.target.value});
+  };
 
-	onDateChange = (event)=> {
-		this.setState({date: event.target.value});
-	};
+  onDateChange = (event)=> {
+    this.setState({date: event.target.value});
+  };
 
-	onMessageChange = (event) => {
-		this.setState({message: event.target.value});
-	};
+  onMessageChange = (event) => {
+    this.setState({message: event.target.value});
+  };
 
-	onSubmitMessage = (event) => {
-		event.preventDefault();
+  onSubmitMessage = (event) => {
+    event.preventDefault();
 
-		const {name, email, tel, data, message} = this.state;
+    const {name, email, tel, data, message} = this.state;
 
-		api.sendMessage({name, email, tel, data, message})
-			.then(() => {
-				this.setState({sendStatus: 'success'});
-				this.setState({showAlert: true});
-			  this.setState({
-					name: '',
-					email: '',
-					tel: '',
-					data: '',
-					message: ''
-			  });
-			})
-			.catch(() => {
-				this.setState({sendStatus: 'error'});
-				this.setState({showAlert: true});
-			}
-		);
-	};
+    api.sendMessage({name, email, tel, data, message})
+      .then(() => {
+        this.setState({sendStatus: 'success'});
+        this.setState({showAlert: true});
+        this.setState({
+          name: '',
+          email: '',
+          tel: '',
+          data: '',
+          message: ''
+        });
+      })
+      .catch(() => {
+        this.setState({sendStatus: 'error'});
+        this.setState({showAlert: true});
+      }
+    );
+  };
 
-	onAlertClose = () => {
-		this.setState({showAlert: false});
-	};
+  onAlertClose = () => {
+    this.setState({showAlert: false});
+  };
 
   render() {
     return (
-    	<>
-				<form
-					className="form"
-					action=""
-					method="post"
-					onSubmit={this.onSubmitMessage}
-				>
-					{FORM_FIELDS.map((item) => (
-						<Input
-							key={item.name}
-							type={item.type}
-							name={item.name}
-							placeholder={item.placeholder}
-							required={item.required}
-							onChange={item.action}
-						/>
-					))}
-					<textarea
-						className="input-text form__textarea"
-						rows="5"
-						placeholder="Your message"
-						name="message"
-						onChange={this.onMessageChange}
-					/>
-					<Button
-						className="btn form__btn"
-						type="submit"
-						name="submit"
-						text="Send message"
-					/>
-				</form>
-				{this.state.showAlert && (
-					<Alert
-						type={this.state.sendStatus}
-						onClose={this.onAlertClose}
-					/>
-				)}
-			</>
+      <>
+        <form
+          className="form"
+          action=""
+          method="post"
+          onSubmit={this.onSubmitMessage}
+        >
+          {FORM_FIELDS.map((item) => (
+            <Input
+              key={item.name}
+              type={item.type}
+              name={item.name}
+              placeholder={item.placeholder}
+              required={item.required}
+              onChange={item.action}
+            />
+          ))}
+          <textarea
+            className="input-text form__textarea"
+            rows="5"
+            placeholder="Your message"
+            name="message"
+            onChange={this.onMessageChange}
+          />
+          <Button
+            className="btn form__btn"
+            type="submit"
+            name="submit"
+            text="Send message"
+          />
+        </form>
+        {this.state.showAlert && (
+          <Alert
+            type={this.state.sendStatus}
+            onClose={this.onAlertClose}
+          />
+        )}
+      </>
     );
   }
 }
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import './Input.less';
 
 const Input = (props) => (
-	<input
-		className={`input-text ${props.className}`}
-		onChange={props.onChange}
-		type={props.type}
-		name={props.name}
-		value={props.value}
-		placeholder={props.placeholder}
-		required={props.required}
-	/>
+  <input
+    className={`input-text ${props.className}`}
+    onChange={props.onChange}
+    type={props.type}
+    name={props.name}
+    value={props.value}
+    placeholder={props.placeholder}
+    required={props.required}
+  />
 );
 
 export default Input;
