81389ba16ba13cce7c4549ce5230439635b9a116
Alisa Filatova <alicefill@bk.ru>
2019-07-24 17:16:57 +0300

    fixed indents

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Button.less';
 
 const Button = ({className, onClick, type = 'text', name, text}) => (
-	<input
-		className={`btn ${className}`}
-		onClick={onClick}
-		type={type}
-		name={name}
-		value={text}
-	/>
+  <input
+    className={`btn ${className}`}
+    onClick={onClick}
+    type={type}
+    name={name}
+    value={text}
+  />
 );
 
 export default Button;
